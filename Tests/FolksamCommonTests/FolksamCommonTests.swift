25edd281c4f0a5f37390f56c50fa0de6723d0924
Rashdan Natiq <rashdan@natiq.se>
2022-03-08 11:18:12 +0100

    Added missing file PolicyDetails.json

--- Tests/FolksamCommonTests/FolksamCommonTests.swift
+++ Tests/FolksamCommonTests/FolksamCommonTests.swift
@@ -6,6 +6,6 @@ final class FolksamCommonTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual("", "Hello, World!")
+        XCTAssertEqual("", "FolksamCommon")
     }
 }
